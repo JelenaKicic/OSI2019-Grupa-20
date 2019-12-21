11982a59c0b41b1e4e7a3eb98606351690285121
vanja kovacevic <kovacevicvanja6@gmail.com>
2019-12-21 19:25:15 +0100

    Postavke projekta-2
    Klasa Time i Event

---
+++ Source/time.cpp
@@ -0,0 +1,12 @@
+#include "Time.h"
+
+Time::Time(int hour, int minute)
+{
+	this->hours = hour;
+	this->minutes = minute;
+}
+
+std::ostream& operator<<(std::ostream& stream, const Time& time)
+{
+	return stream << time.hours << ':' << time.minutes;
+}
